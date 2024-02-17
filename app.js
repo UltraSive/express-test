5cd18fb652ef05484ea8bb81a01757f7131da96a
ultrasive <github@aidanperry.net>
2024-02-16 18:49:53 -0600

    created blank express.js project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+// Parse JSON bodies for this app.
+app.use(express.json());
+
+// MongoDB connection URI with credentials
+app.get('/', (req, res) => {
+
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
